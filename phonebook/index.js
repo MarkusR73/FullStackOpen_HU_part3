0bb5b6d6ebc671cef2516f4bedcbe2a5402c5c8b
MarkusR73 <markus.rautell@gmail.com>
2025-01-15 13:34:02 +0200

    Solution for exercise 3.6

--- phonebook/index.js
+++ phonebook/index.js
@@ -64,7 +64,13 @@ app.post('/api/persons', (request, response) => {
     
     if (!body.name || !body.number) {
         return response.status(400).json({ 
-            error: 'content missing' 
+            error: 'name or number missing' 
+        })
+    }
+    const dublicate = persons.find(person => person.name === body.name)
+    if (dublicate) {
+        return response.status(400).json({ 
+            error: 'name must be unique' 
         })
     }
   
