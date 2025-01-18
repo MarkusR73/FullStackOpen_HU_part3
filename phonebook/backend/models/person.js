209e07a89323cd4977a7c6fa65f115df9ff6326d
MarkusR73 <markus.rautell@gmail.com>
2025-01-18 16:29:18 +0200

    Solution for exercise 3.20

--- phonebook/backend/models/person.js
+++ phonebook/backend/models/person.js
@@ -23,8 +23,18 @@ const personSchema = new mongoose.Schema({
     },
     number: {
         type: String,
-        minLength: 3,
-        required: true
+        // '-' is included in the lenght -> at least 8 digits + '-'
+        minLength: 9,
+        required: true,
+        validate: {
+            validator: function(v) {
+                // \d{2,3}: The first part has 2 or 3 digits
+                // -: A hyphen separates the two parts
+                // \d{5,-}: The second part has at least 5 digits
+                const phoneDigits = /^(?:\d{2,3}-\d{5,})$/
+                return phoneDigits.test(v)
+            }
+        }
     }
 })
 
